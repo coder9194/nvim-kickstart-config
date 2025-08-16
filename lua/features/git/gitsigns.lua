755d98bd1b35eef250d262e0c432cc704368c4d2
coder9194 <cckk.hk@gmail.com>
2025-08-17 00:43:30 +0800

    fix gitsigns signs and event

--- lua/features/git/gitsigns.lua
+++ lua/features/git/gitsigns.lua
@@ -4,13 +4,22 @@
 
 return {
   'lewis6991/gitsigns.nvim',
+  event = 'VeryLazy',
   opts = {
     signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
+      untracked = { text = '▎' },
+    },
+    signs_staged = {
+      add = { text = '▎' },
+      change = { text = '▎' },
+      delete = { text = '' },
+      topdelete = { text = '' },
+      changedelete = { text = '▎' },
     },
   },
   -- stylua: ignore
