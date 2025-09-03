99f6a877924723a0b62d10735cfae77ab990a579
coder9194 <cckk.hk@gmail.com>
2025-09-04 00:30:04 +0800

    fix current branch git log

--- lua/features/git/gv.lua
+++ lua/features/git/gv.lua
@@ -1,10 +1,15 @@
 -- A git commit browser
 -- https://github.com/junegunn/gv.vim
 
+function show_current_branch_log()
+  local output = require('utils.nvim').get_command_output 'echo gitbranch#name()'
+  vim.cmd('GV --first-parent ' .. output)
+end
+
 return {
   'junegunn/gv.vim',
   -- stylua: ignore
   keys = {
-    { "<leader>ggc", mode = "n", function() require("utils.gv-vim").show_current_branch_log() end, desc = "Git Log of Current Branch" },
+    { "<leader>ggc", mode = "n", show_current_branch_log, desc = "Git Log of Current Branch" },
   },
 }
---
+++ lua/features/git/vim-gitbranch.lua
@@ -0,0 +1,6 @@
+-- This plugin provides a function which returns the name of the git branch.
+-- https://github.com/itchyny/vim-gitbranch
+
+return {
+  'itchyny/vim-gitbranch',
+}
