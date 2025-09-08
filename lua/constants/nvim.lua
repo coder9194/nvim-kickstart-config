b5110511909cc951a97641b1b8b75634be38f53c
coder9194 <cckk.hk@gmail.com>
2025-09-08 21:09:08 +0800

    set cursor color by mode

--- lua/constants/nvim.lua
+++ lua/constants/nvim.lua
@@ -2,4 +2,12 @@ local M = {}
 
 M.WAITING_TIME = 100
 
+-- Most colors are got from sonokai theme
+M.MODE_COLORS = {
+  NORMAL = '#85dad2',
+  NORMAL_CURSOR_LINE = '#424f4c',
+  INSERT = '#adda78',
+  VISUAL = '#fd7883',
+}
+
 return M
--- lua/features/ui/modes.lua
+++ lua/features/ui/modes.lua
@@ -1,21 +1,43 @@
--- Set cursor & cursor line color based on mode
+local normal_cursor_color = require('constants.nvim').MODE_COLORS.NORMAL
+local normal_cursor_line_color = require('constants.nvim').MODE_COLORS.NORMAL_CURSOR_LINE
+local insert_cursor_color = require('constants.nvim').MODE_COLORS.INSERT
+local visual_cursor_color = require('constants.nvim').MODE_COLORS.VISUAL
 
+local function set_colors_by_mode()
+  local new_mode = vim.fn.mode()
+  if new_mode == 'n' then
+    vim.api.nvim_set_hl(0, 'Cursor', { bg = normal_cursor_color, fg = '#ffffff' })
+    vim.api.nvim_set_hl(0, 'CursorLine', { bg = normal_cursor_line_color })
+  elseif new_mode == 'i' then
+    vim.api.nvim_set_hl(0, 'Cursor', { bg = insert_cursor_color, fg = '#000000' })
+  elseif new_mode == 'v' or new_mode == 'V' or new_mode == '\22' then
+    vim.api.nvim_set_hl(0, 'Cursor', { bg = visual_cursor_color, fg = '#000000' })
+  end
+end
+
+-- Auto command to trigger on mode changes
+vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
+  pattern = '*',
+  callback = function()
+    set_colors_by_mode()
+  end,
+})
+
+-- Set cursor & cursor line color based on mode
 return {
   'mvllow/modes.nvim',
   event = 'VeryLazy',
   config = function()
-    vim.cmd 'highlight Cursor guibg=#85dad2'
-    vim.cmd 'highlight CursorLine guibg=#424F4C'
+    set_colors_by_mode()
     require('modes').setup {
       colors = {
-        copy = '#85dad2',
-        delete = '#c75c6a',
-        insert = '#adda78',
-        visual = '#fd6883',
+        copy = require('constants.nvim').MODE_COLORS.NORMAL,
+        insert = require('constants.nvim').MODE_COLORS.INSERT,
+        visual = require('constants.nvim').MODE_COLORS.VISUAL,
       },
-      line_opacity = 0.2,
-      skt_cursor = true,
-      set_cursorline = false,
+      line_opacity = 0.3,
+      set_cursor = true,
+      set_cursorline = false, -- Keep cursor line shown on other windows
     }
   end,
 }
