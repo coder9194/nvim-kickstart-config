9ec1bd96a230420c5f24108983796f7fa9606904
coder9194 <cckk.hk@gmail.com>
2025-09-13 20:01:29 +0800

    fix loading of workspace script

--- lua/features/automation/overseer.lua
+++ lua/features/automation/overseer.lua
@@ -1,17 +1,32 @@
 -- A task runner and job management plugin for Neovim
 -- https://github.com/stevearc/overseer.nvim
 
-local base_templates = { 'builtin' }
+local function load_current_workspace_templates()
+  local base_templates = { 'builtin' }
+  local current_workspace_name = require('utils.nvim').get_current_workspace_name()
+  local has_current_workspace_template = require('utils.overseer').check_has_workspace_template(current_workspace_name)
+  local current_workspace_templates = has_current_workspace_template and require('utils.overseer').get_workspace_templates(current_workspace_name) or {}
 
-local current_workspace_name = require('utils.nvim').get_current_workspace_name()
-local has_current_workspace_template = require('utils.overseer').check_has_workspace_template(current_workspace_name)
-local current_workspace_templates = has_current_workspace_template and require('utils.overseer').get_workspace_templates(current_workspace_name) or {}
+  for _, workspace_template in ipairs(current_workspace_templates) do
+    require('overseer').load_template(workspace_template)
+  end
+end
 
 return {
   'stevearc/overseer.nvim',
   config = function()
     -- Register custom component
     require 'overseer.component.my_component.init'
+
+    require('overseer').setup {
+      task_list = {
+        bindings = {
+          ['<C-f>'] = require('utils.overseer').open_floating_window,
+        },
+      },
+    }
+
+    load_current_workspace_templates()
   end,
   opts = {
     task_list = {
