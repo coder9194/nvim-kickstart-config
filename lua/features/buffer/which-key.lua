ac62813fd4357eeb5f761780a8e9742405d8fc97
coder9194 <cckk.hk@gmail.com>
2025-09-14 23:22:06 +0800

    fix deletion invisible buffers

--- lua/features/buffer/which-key.lua
+++ lua/features/buffer/which-key.lua
@@ -10,7 +10,7 @@ return {
   },
   -- stylua: ignore
   keys = {
-    { '<leader>bh', function() require('utils.buffer').delete_background_buffers() end, desc = 'Delete background buffers', },
+    { '<leader>bD', function() require('utils.buffer').delete_invisible_buffers() end, desc = 'Delete invisible buffers', },
     { '<leader>bp', function() require('utils.window').set_current_window_buffer(vim.g.custom_buffer_yank_id) end, desc = 'Paste current buffer by yanked buffer', },
     { '<leader>br', function() vim.cmd 'e!' vim.lsp.inlay_hint.enable() end, desc = 'Buffer reload', },
     { '<leader>bt', function() local position_in_line, line_number = require('utils.nvim').get_cursor_position() vim.cmd 'tabe %' require('utils.nvim').set_cursor_position(position_in_line, line_number) end, desc = 'Open current buffer into new tab', },
--- lua/utils/buffer.lua
+++ lua/utils/buffer.lua
@@ -16,10 +16,63 @@ function M.get_file_path(buffer_id)
   return vim.api.nvim_buf_get_name(buffer_id)
 end
 
--- Delete background buffers
-function M.delete_background_buffers()
+-- Check if buffer is shown in any window of any tab
+---@param buffer_id integer
+function M.is_visible(buffer_id)
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_buf(win) == buffer_id then
+      return true
+    end
+  end
+  return false
+end
+
+-- Get buffer flags
+function M.get_buffer_flags(buf)
+  local flags = ''
+
+  -- current / alternate
+  if buf == vim.api.nvim_get_current_buf() then
+    flags = flags .. '%'
+  elseif buf == vim.fn.bufnr '#' then
+    flags = flags .. '#'
+  else
+    flags = flags .. ' '
+  end
+
+  -- active / hidden
+  if vim.fn.bufwinnr(buf) ~= -1 then
+    flags = flags .. 'a'
+  elseif vim.api.nvim_buf_is_loaded(buf) then
+    flags = flags .. 'h'
+  else
+    flags = flags .. ' '
+  end
+
+  -- readonly
+  if vim.bo[buf].readonly then
+    flags = flags .. '='
+  end
+
+  -- modified
+  if vim.bo[buf].modified then
+    flags = flags .. '+'
+  end
+
+  -- not modifiable
+  if not vim.bo[buf].modifiable then
+    flags = flags .. '-'
+  end
+
+  return flags
+end
+
+-- Delete invisible buffers
+function M.delete_invisible_buffers()
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.api.nvim_buf_is_loaded(buf) and vim.fn.bufwinnr(buf) == -1 then
+    local is_buffer_active = M.get_buffer_flags(buf):find 'a' ~= nil
+
+    if vim.api.nvim_buf_is_valid(buf) and not M.is_visible(buf) and not is_buffer_active then
       vim.api.nvim_buf_delete(buf, { force = true })
     end
   end
