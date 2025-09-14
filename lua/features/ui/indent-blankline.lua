1578d490953a1b5e21c305b93faa3ab8dc72b731
coder9194 <cckk.hk@gmail.com>
2025-09-14 17:53:59 +0800

    fix indent highlight by indent-blankline.nvim

--- lua/features/ui/indent-blankline.lua
+++ lua/features/ui/indent-blankline.lua
@@ -5,7 +5,9 @@ return {
   dependiences = {
     'HiPhish/rainbow-delimiters.nvim',
   },
+
   main = 'ibl',
+  event = 'UIEnter',
   config = function()
     local highlight = {
       'RainbowRed',
@@ -30,21 +32,69 @@ return {
       vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
     end)
 
-    local javascript_nodes = { 'object', 'array', 'return_statement' }
+    local javascript_nodes = {
+      -- objects/collections
+      'object',
+      'pair',
+      'array',
+      'object_pattern',
+      'array_pattern',
+
+      -- properties / assignments
+      'property_identifier',
+      'property',
+      'property_assignment',
+      'shorthand_property_identifier',
+
+      -- functions / calls / member access
+      'call_expression',
+      'new_expression',
+      'arguments',
+      'arrow_function',
+      'function_declaration',
+      'function_expression',
+      'parenthesized_expression',
+      'subscript_expression',
+
+      -- module / import
+      'import_statement',
+      'import_specifier',
+      'import_call',
+      'require_call',
+
+      -- control + returns
+      'return_statement',
+      'if_statement',
+      'for_statement',
+      'while_statement',
+      'switch_statement',
+
+      -- JSX / TSX
+      'jsx_element',
+      'jsx_self_closing_element',
+      'jsx_fragment',
+      'jsx_expression',
+      'jsx_attribute',
+
+      -- generic
+      'program',
+      'expression_statement',
+      'variable_declaration',
+      'variable_declarator',
+    }
 
     vim.g.rainbow_delimiters = { highlight = highlight }
     require('ibl').setup {
       scope = {
         highlight = highlight,
         -- Add extra nodes of different languages here which are not highlighted by default
-        -- `node_type` is the file extension
+        -- `node_type` can be found in the first table (right value) of `https://github.com/nvim-treesitter/nvim-treesitter/blob/master/lua/nvim-treesitter/parsers.lua`
         -- (node keys can be found in `InspectTree`, e.g. `table_constructor` in `value: (table_constructor ; [9, 22] - [17, 5]`)
         include = {
           node_type = {
             lua = { 'return_statement', 'table_constructor' },
-            js = javascript_nodes,
-            ts = javascript_nodes,
-            jsx = javascript_nodes,
+            javascript = javascript_nodes,
+            typescript = javascript_nodes,
             tsx = javascript_nodes,
           },
         },
