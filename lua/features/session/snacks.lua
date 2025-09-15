bdb9fc99b8781cda8b749e0f066f04e7795d5625
Jerry Lam <jerry.lam@aetoscg.com>
2025-09-15 10:13:07 +0800

    fix duplicated dashboard by snacks.nvim

--- lua/features/session/snacks.lua
+++ lua/features/session/snacks.lua
@@ -1,14 +1,6 @@
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    local is_no_file_opened = vim.fn.argc() == 0
-    if is_no_file_opened then
-      require('snacks').dashboard()
-    end
-  end,
-})
-
 return {
   'folke/snacks.nvim',
+  lazy = false,
   opts = {
     dashboard = {
       enabled = true,
