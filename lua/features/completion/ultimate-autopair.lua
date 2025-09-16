a8ba5dca310357913bb067abeaacc9304ec67770
Jerry Lam <jerry.lam@aetoscg.com>
2025-09-16 14:32:26 +0800

    add auto pairing by ultimate-autopair.nvim

---
+++ lua/features/completion/ultimate-autopair.lua
@@ -0,0 +1,8 @@
+-- Auto pair function (e.g. add closing bracket)
+-- https://github.com/altermo/ultimate-autopair.nvim
+
+return {
+  'altermo/ultimate-autopair.nvim',
+  event = { 'InsertEnter', 'CmdlineEnter' },
+  opts = {},
+}
