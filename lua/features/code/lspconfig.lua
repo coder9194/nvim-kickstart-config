8a4644a95f1cff17aec77d23c022eb4ec65a4780
Jerry Lam <jerry.lam@aetoscg.com>
2025-09-16 14:45:06 +0800

    add source actions keymap

--- lua/features/code/lspconfig.lua
+++ lua/features/code/lspconfig.lua
@@ -1,7 +1,9 @@
 return {
   'neovim/nvim-lspconfig',
+  -- stylua: ignore
   keys = {
     { '<leader>cr', vim.lsp.buf.rename, desc = 'Rename Variable' },
     { '<leader>ca', vim.lsp.buf.code_action, desc = 'Code Actions' },
+    { '<leader>cA', function() vim.lsp.buf.code_action { context = { diagnostics = {}, only = { 'source' } } } end, desc = 'Source Actions', },
   },
 }
