4b4a957cea7ae4561d8851867e519d9697967766
Jerry Lam <jerry.lam@aetoscg.com>
2025-09-30 17:55:51 +0800

    workaround for keymap suggestion is not shown when macro mode is entered

--- lua/features/ui/lualine.lua
+++ lua/features/ui/lualine.lua
@@ -1,5 +1,19 @@
 vim.o.cmdheight = 0 -- Hide command line when not being used
 
+-- TODO: find a way to remove this workaround
+vim.api.nvim_create_autocmd('RecordingEnter', {
+  desc = 'Show command line when recording macros',
+  callback = function()
+    vim.opt.cmdheight = 1
+  end,
+})
+vim.api.nvim_create_autocmd('RecordingLeave', {
+  desc = 'Hide command line when not recording macros',
+  callback = function()
+    vim.opt.cmdheight = 0
+  end,
+})
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
