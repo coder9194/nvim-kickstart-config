f51d66652f212225be8131a4d76e780150dab495
coder9194 <cckk.hk@gmail.com>
2025-10-06 18:06:24 +0800

    show current line number

--- lua/features/ui/options.lua
+++ lua/features/ui/options.lua
@@ -1,7 +1,8 @@
 vim.api.nvim_create_autocmd('VimEnter', {
   desc = 'Load options when Vim is started',
   callback = function()
-    vim.o.relativenumber = true
+    vim.opt.number = true
+    vim.opt.relativenumber = true
   end,
 })
 
