dac5980cb7acbd8cc8016cc48aac29f1425a97b4
Jerry Lam <jerry.lam@aetoscg.com>
2025-11-06 15:06:26 +0800

    improve delete buffer cmd

--- lua/features/buffer/snacks.lua
+++ lua/features/buffer/snacks.lua
@@ -3,7 +3,7 @@ return {
   -- stylua: ignore
   keys = {
     { '<leader>bb', "<cmd>e #<cr>", desc = 'Last active buffer', },
-    { '<leader>bd', function() require('snacks').bufdelete() end, desc = 'Delete Current Buffer', },
+    { '<leader>bd', function() require('snacks').bufdelete() vim.cmd 'close' end, desc = 'Delete Current Buffer', },
     { '<leader>bg', function() require('snacks').picker.grep_buffers() end, desc = 'Grep Opened Buffers', },
     { '<leader>bs', function() require('snacks').picker.buffers() end, desc = 'Buffers', },
   },
