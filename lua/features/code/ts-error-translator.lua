383496947387697c9fba13d57d1ac2c7f691b5ed
Jerry Lam <jerry.lam@aetoscg.com>
2025-11-18 10:07:07 +0800

    add ts-error-translator.nvim

---
+++ lua/features/code/ts-error-translator.lua
@@ -0,0 +1,6 @@
+-- A Neovim port of [Matt Pocock's ts-error-translator for VSCode](https://github.com/mattpocock/ts-error-translator) for turning messy and confusing TypeScript errors into plain English.
+-- https://github.com/dmmulroy/ts-error-translator.nvim
+return {
+  'dmmulroy/ts-error-translator.nvim',
+  opts = {},
+}
