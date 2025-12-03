dc712d91c1630fdeb9697175f9027bc6d96f8674
Jerry Lam <jerry.lam@aetoscg.com>
2025-12-03 14:16:34 +0800

    fix invalid buffer id

--- lua/features/lsp/configs/utils.lua
+++ lua/features/lsp/configs/utils.lua
@@ -4,13 +4,17 @@ M.use_codelens = function(client, bufnr)
   local is_codelens_supported = client.server_capabilities.codeLensProvider
   if is_codelens_supported then
     -- Initially load codelens
-    vim.lsp.codelens.refresh { bufnr = bufnr }
+    if vim.api.nvim_buf_is_valid(bufnr) then
+      vim.lsp.codelens.refresh { bufnr = bufnr }
+    end
 
     -- Refresh codelens on the fly
     vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave' }, {
       desc = 'Refresh CodeLens on the fly',
       callback = function()
-        vim.lsp.codelens.refresh { bufnr = bufnr }
+        if vim.api.nvim_buf_is_valid(bufnr) then
+          vim.lsp.codelens.refresh { bufnr = bufnr }
+        end
       end,
     })
   end
