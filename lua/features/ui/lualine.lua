2508f62b45b5f911afe4b5ad6084a97498471ca6
coder9194 <cckk.hk@gmail.com>
2026-06-29 01:19:32 +0800

    show search index and occurrence in status bar

--- lua/features/ui/lualine.lua
+++ lua/features/ui/lualine.lua
@@ -2,6 +2,24 @@
 
 vim.o.cmdheight = 0 -- Hide command line when not being used
 
+local function search_result()
+  -- Only execute tracking logic if search highlighting is currently active
+  if vim.v.hlsearch == 0 then
+    return ''
+  end
+
+  -- Safely call Neovim's internal search calculator
+  local res = vim.fn.searchcount { maxcount = 999, timeout = 500 }
+
+  -- If there are no occurrences or formatting properties are empty, return blank
+  if res.total == 0 then
+    return ''
+  end
+
+  -- Format output string exactly as [current/total]
+  return string.format(' [%d/%d]', res.current, res.total)
+end
+
 -- TODO: find a way to remove this workaround
 vim.api.nvim_create_autocmd('RecordingEnter', {
   desc = 'Show command line when recording macros',
@@ -23,6 +41,18 @@ return {
   opts = {
     options = {
       globalstatus = true, -- enable global single statusline at neovim bottom
+    },
+
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+
+      -- 2. Inject the custom search component into your preferred section
+      lualine_x = { search_result, 'encoding', 'fileformat', 'filetype' },
+
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
     },
   },
 }
