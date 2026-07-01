78becaa81a8658d2af4b35dc4d9a157cb76e10bf
coder9194 <cckk.hk@gmail.com>
2026-07-02 01:03:19 +0800

    focus on popup on git blame line

--- lua/features/git/gitsigns.lua
+++ lua/features/git/gitsigns.lua
@@ -2,6 +2,23 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 -- https://github.com/lewis6991/gitsigns.nvim
 
+local show_blame_line_popup_and_focus = function()
+  -- 1. Preserve Neovim's original window creation function
+  local original_open_win = vim.api.nvim_open_win
+
+  -- 2. Intercept the function to force focus
+  vim.api.nvim_open_win = function(buf, enter, config)
+    -- Force the 'enter' argument to true so the cursor jumps inside immediately
+    local win = original_open_win(buf, true, config)
+
+    -- Instantly self-heal and restore the original Neovim function
+    vim.api.nvim_open_win = original_open_win
+    return win
+  end
+
+  require('gitsigns.actions').blame_line { full = true }
+end
+
 return {
   'lewis6991/gitsigns.nvim',
   event = 'VeryLazy',
@@ -28,7 +45,7 @@ return {
     { 'Hh', function() require('gitsigns').nav_hunk 'prev' end, desc = 'Preview hunk', },
 ---@diagnostic disable-next-line: param-type-mismatch
     { 'Lh', function() require('gitsigns').nav_hunk 'next' end, desc = 'Next hunk', },
-    { '<leader>gbl', mode = 'n', function() require('gitsigns.actions').blame_line() end, desc = 'Blame Line' },
+    { '<leader>gbl', mode = 'n', show_blame_line_popup_and_focus, desc = 'Blame Line' },
     { '<leader>ghs', mode = 'n', function() require('gitsigns.actions').stage_hunk() end, desc = 'Stage/Unstage hunk' },
     { '<leader>ghS', mode = 'n', function() require('gitsigns.actions').stage_buffer() end, desc = 'Stage file hunks' },
     { '<leader>ghr', mode = 'n', function() require('gitsigns.actions').reset_hunk() end, desc = 'Reset hunk' },
