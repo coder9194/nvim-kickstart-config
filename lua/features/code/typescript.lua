0953c26190f2ad3711fcd4eb6bf2dc705cf02c7d
coder9194 <cckk.hk@gmail.com>
2026-08-19 00:36:47 +0800

    refactor typescript config

---
+++ lua/features/code/typescript.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        tsc = {
+          cmd = { 'tsc', '--lsp', '--stdio' }, -- Explicitly invoke global tsc, skipping project node_modules
+          on_attach = function(client, bufnr)
+            if client.supports_method 'textDocument/codeLens' then
+              vim.lsp.codelens.refresh { bufnr = bufnr }
+
+              -- Refresh CodeLens when leaving insert mode or holding cursor
+              vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'CursorHold' }, {
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.codelens.refresh { bufnr = bufnr }
+                end,
+              })
+            end
+          end,
+          settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = {
+                  enabled = 'all',
+                },
+                parameterTypes = { enabled = false },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                enumMemberValues = { enabled = false },
+              },
+              referencesCodeLens = {
+                enabled = true,
+                showOnAllFunctions = true,
+              },
+              implementationsCodeLens = {
+                enabled = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  -- https://github.com/dmmulroy/tsc.nvim
+  {
+    'dmmulroy/tsc.nvim',
+    config = true,
+    opts = {
+      use_trouble_qflist = true,
+    },
+    keys = {
+      { '<leader>cdt', mode = 'n', '<cmd>TSC<cr>', desc = 'Workspace type diagnostic' },
+    },
+  },
+}
