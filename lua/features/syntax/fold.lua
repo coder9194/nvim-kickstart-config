f0b6a887f06f393df70ca39f2902e59b121e74a3
coder9194 <cckk.hk@gmail.com>
2026-08-21 18:31:08 +0800

    use nvim-ufo for performant folding

--- lua/features/syntax/fold.lua
+++ lua/features/syntax/fold.lua
@@ -1,24 +1,12 @@
-vim.opt.foldenable = true -- Enable folding
-
--- Everything stays open and visible by default.
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 99
-
-vim.api.nvim_create_autocmd('FileType', {
-  group = vim.api.nvim_create_augroup('TreesitterFolding', { clear = true }),
-  callback = function(args)
-    local bufnr = args.buf
-    local line_count = vim.api.nvim_buf_line_count(bufnr)
-
-    -- Bypass Treesitter folding on huge files to prevent normal mode lag
-    if line_count > 5000 then
-      return
-    end
-
-    -- Set window-local folding options safely
-    vim.wo.foldmethod = 'expr'
-    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-  end,
-})
-
-return {}
+return {
+  {
+    {
+      'kevinhwang91/nvim-ufo',
+      dependencies = {
+        'kevinhwang91/promise-async',
+        'neovim/nvim-lspconfig',
+      },
+      config = true,
+    },
+  },
+}
