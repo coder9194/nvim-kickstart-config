0087aff25e87c430258e68ea195397039b8a129d
coder9194 <cckk.hk@gmail.com>
2026-08-28 17:51:51 +0800

    fix scrolling lag

--- lua/features/ui/neoscroll.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/karb94/neoscroll.nvim
-return {
-  'karb94/neoscroll.nvim',
-  opts = {
-    duration_multiplier = 0.5,
-  },
-}
---
+++ lua/features/ui/scrolling.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'nvim-mini/mini.animate',
+    version = false,
+    opts_extend = {
+      'scroll',
+    },
+    opts = {
+      scroll = {
+        enable = true,
+        time = 300,
+      },
+    },
+  },
+}
