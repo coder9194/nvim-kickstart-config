e00e9c023f6c5818126ee974925cadf5e95e4196
coder9194 <cckk.hk@gmail.com>
2026-08-31 09:59:56 +0800

    fix fold is collapsed when buffer is opened

--- lua/features/syntax/fold.lua
+++ lua/features/syntax/fold.lua
@@ -6,7 +6,14 @@ return {
         'kevinhwang91/promise-async',
         'neovim/nvim-lspconfig',
       },
-      config = true,
+      config = function()
+        vim.o.foldcolumn = '1' -- '0' is not bad
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+
+        require('ufo').setup()
+      end,
     },
   },
 }
